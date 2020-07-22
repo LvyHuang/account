ad39f94e7dabdd424cbc397ec21a21542dc20a1e
LvyHuang <1903757279@qq.com>
2020-07-22 23:46:28 +0800

    增加Money组件的标签

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,6 +1,47 @@
 <template>
     <Layout>
-        Money.vue
+        <div>
+            <ul class="types">
+                <li class="selected">支出</li>
+                <li>收入</li>
+            </ul>
+        </div>
+        <div class="tags">
+            <ul class="current"></ul>
+                <span>衣</span>
+                <span>食</span>
+                <span>住</span>
+                <span>行</span>
+            <div class="new">
+                <button>新增标签</button>
+            </div>
+        </div>
+        <div>
+            <label class="notes">
+                <span class="name">备注</span>
+                <input type="text">
+            </label>
+        </div>
+        <div class="nubmerPad">
+            <div class="output">100</div>
+            <div class="buttons">
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>+</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>-</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>完成</button>
+                <button>.</button>
+                <button>0</button>
+                <button>清空</button>
+            </div>
+        </div>
     </Layout>
 </template>
 
@@ -11,5 +52,4 @@
 </script>
 
 <style lang="scss" scoped>
-
 </style>

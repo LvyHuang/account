b5cab04b755004758b43eb2071febfdea10580c1
LvyHuang <1903757279@qq.com>
2020-07-27 23:51:37 +0800

    模块化:Money组件分成四个子组件

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,53 +1,20 @@
 <template>
     <Layout class-prefix="layout">
-        <div class="nubmerPad">
-            <div class="output">100</div>
-            <div class="buttons">
-                <button>1</button>
-                <button>2</button>
-                <button>3</button>
-                <button>删除</button>
-                <button>4</button>
-                <button>5</button>
-                <button>6</button>
-                <button>清空</button>
-                <button>7</button>
-                <button>8</button>
-                <button>9</button>
-                <button class="ok">完成</button>
-                <button class="zero">0</button>
-                <button>.</button>
-            </div>
-        </div>
-        <div>
-            <label class="notes">
-                <span class="name">备注</span>
-                <input type="text" placeholder="在这里输入备注">
-            </label>
-        </div>
-        <div class="tags">
-            <ul class="current">
-                <li>衣</li>
-                <li>食</li>
-                <li>住</li>
-                <li>行</li>
-            </ul>
-            <div class="new">
-                <button>新增标签</button>
-            </div>
-        </div>
-        <div>
-            <ul class="types">
-                <li class="selected">支出</li>
-                <li>收入</li>
-            </ul>
-        </div>
+        <NumberPad/>
+        <Notes/>
+        <Tags/>
+        <Types/>
     </Layout>
 </template>
 
 <script lang="ts">
+  import NumberPad from '@/components/Money/NumberPad.vue';
+  import Notes from '@/components/Money/Notes.vue';
+  import Tags from '@/components/Money/Tags.vue';
+  import Types from '@/components/Money/Types.vue';
   export default {
     name: 'Money',
+      components: {Types, Tags, Notes, NumberPad},
   };
 </script>
 
@@ -55,123 +22,5 @@
     .layout-content{
         display: flex;
         flex-direction: column-reverse;
-    }
-</style>
-<style lang="scss" scoped>
-    @import "~@/assets/style/helper.scss";
-    .types{
-        background: #FFDA47;
-        display: flex;
-        text-align: center;
-        height: 60px;
-        font-size: 20px;
-        > li{
-            width: 50%;
-            height: 60px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            position: relative;
-            &.selected::after{
-                content: '';
-                position: absolute;
-                bottom: 0;
-                left: 50%;
-                transform: translate(-50%,0);
-                width: 80px;
-                height: 2px;
-                background: #333;
-            }
-        }
-    }
-    .tags{
-        flex-grow: 1;
-        font-size: 14px;
-        padding: 16px;
-        > .current{
-            display: flex;
-            > li{
-                background: #d9d9d9;
-                height: 24px;
-                line-height: 24px;
-                border-radius: 12px;
-                padding: 0 16px;
-                margin-right: 12px;
-            }
-        }
-        > .new{
-            padding-top: 16px;
-            button{
-                background: transparent;
-                border: none;
-                color: #999;
-                border-bottom: 1px solid;
-                padding: 0 4px;
-            }
-        }
-    }
-    .notes{
-        font-size: 14px;
-        padding-left: 16px;
-        display: flex;
-        align-items: center;
-        .name{
-            padding-right: 16px;
-        }
-        input{
-            height: 50px;
-            flex-grow: 1;
-            background: transparent;
-            border: none;
-            padding-right: 16px;
-        }
-    }
-    .nubmerPad{
-        .output{
-            font-size: 32px;
-            background: #f5f5f5;
-            font-family: Consolas, monospace;
-            padding: 5px 16px;
-            text-align: right;
-        }
-        .buttons{
-            @extend %clearFix;
-            > button{
-                width: 25%;
-                height: 56px;
-                float: left;
-                background: transparent;
-                border: none;
-                &.ok{
-                    height: 56*2px;
-                    float: right;
-                }
-                &.zero{
-                    width: 25*2%;
-                }
-                $bg: #f2f2f2;
-                &:nth-child(1){
-                    background: #f2f2f2;
-                }
-                &:nth-child(2),&:nth-child(5){
-                    background: darken($bg, 4%);
-                }
-                &:nth-child(3),&:nth-child(6),&:nth-child(9){
-                    background: darken($bg, 4*2%);
-                }
-                &:nth-child(4),&:nth-child(7),&:nth-child(10){
-                    background: darken($bg, 4*3%);
-                }
-                &:nth-child(8),&:nth-child(11),&:nth-child(13){
-                    background: darken($bg, 4*4%);
-                }
-                &:nth-child(14){
-                    background: darken($bg, 4*5%);
-                }
-                &:nth-child(12){
-                    background: darken($bg, 4*6%);
-                }
-            }
-        }
     }
 </style>

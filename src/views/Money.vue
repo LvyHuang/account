c0c6c704e18303fce6318bc14a2bfb3cada9bb6f
LvyHuang <1903757279@qq.com>
2020-08-12 23:55:17 +0800

    使用localStorage存储数据

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,8 +1,8 @@
 <template>
     <Layout class-prefix="layout">
-        <NumberPad :value.sync="record.amount" @update:value="onUpdateAmount"/>
+        <NumberPad :value.sync="record.amount" @update:value="onUpdateAmount" @submit="saveRecord"/>
         <Notes @update:value="onUpdateNotes"/>
-        {{record}}
+        {{recordList}}
         <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
         <Types :value.sync="record.type"/>
     </Layout>
@@ -14,13 +14,14 @@
   import Notes from '@/components/Money/Notes.vue';
   import Tags from '@/components/Money/Tags.vue';
   import Types from '@/components/Money/Types.vue';
-  import {Component} from 'vue-property-decorator';
+  import {Component, Watch} from 'vue-property-decorator';
 
   type Record = {
       tags: string[],
       notes: string,
       type: string,
-      amount: number
+      amount: number,
+      createdAt?: Date   //类 / 构造函数
   }
 
   @Component({
@@ -28,6 +29,7 @@
   })
   export default class Money extends Vue{
       tags = ['餐饮','购物','房租','交通','娱乐','医疗'];
+      recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
       record: Record = {
           tags: [], notes: '', type: '-', amount: 0
       };
@@ -38,11 +40,18 @@
       onUpdateNotes(value: string){
         this.record.notes = value;
       }
-      onUpdateType(value: string){
-        this.record.type = value;
-      }
       onUpdateAmount(value: string){
         this.record.amount = parseFloat(value);
+      }
+      saveRecord(){
+          const record2: Record = JSON.parse(JSON.stringify(this.record));
+          record2.createdAt = new Date();
+          this.recordList.push(record2);
+      }
+
+      @Watch('recordList')
+      onRecordListChange(){
+          window.localStorage.setItem('recordList',JSON.stringify(this.recordList))
       }
   };
 </script>

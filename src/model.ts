90634e51e4ef9d668b7f13502c1438a1868123fd
LvyHuang <1903757279@qq.com>
2020-08-17 23:38:03 +0800

    封装出一个model

---
+++ src/model.ts
@@ -0,0 +1,9 @@
+const model = {
+  fetch(){
+    return JSON.parse(window.localStorage.getItem('recordList') || '[]') as RecordItem[];
+  },
+  save(data: RecordItem[]){
+    window.localStorage.setItem('recordList',JSON.stringify(data));
+  }
+};
+export default model

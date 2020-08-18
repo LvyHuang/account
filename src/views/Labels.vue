8628e6d5ca38347f8c763ff14b821fd754227817
LvyHuang <1903757279@qq.com>
2020-08-18 23:32:15 +0800

    Labels的样式

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,6 +1,18 @@
 <template>
     <Layout>
-        Labels.vue
+        <ol class="tags">
+            <li>衣<Icon name="right"/>
+            </li>
+            <li>食<Icon name="right"/>
+            </li>
+            <li>住<Icon name="right"/>
+            </li>
+            <li>行<Icon name="right"/>
+            </li>
+        </ol>
+        <div class="createTag-wrapper">
+            <button class="createTag">新建标签</button>
+        </div>
     </Layout>
 </template>
 
@@ -11,5 +23,35 @@
 </script>
 
 <style lang="scss" scoped>
-
+.tags{
+    background: white;
+    font-size: 16px;
+    padding-left: 16px;
+    >li{
+        min-height: 44px;
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        border-bottom: 1px solid #e6e6e6;
+        svg{
+            width: 18px;
+            height: 18px;
+            color: #666666;
+            margin-right: 16px;
+        }
+    }
+}
+.createTag{
+    background: #767676;
+    color: white;
+    border-radius: 4px;
+    border: none;
+    height: 40px;
+    padding: 0 16px;
+    &-wrapper{
+        text-align: center;
+        padding: 16px;
+        margin-top: 44-16px;
+    }
+}
 </style>
